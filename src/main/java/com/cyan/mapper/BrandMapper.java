13afa776f5c5caf2cd7fdf748d5194fb5a00b2a4
Cyan <204267667@qq.com>
2022-09-05 20:03:53 +0800

    查询brand所有&&结果映射

---
+++ src/main/java/com/cyan/mapper/BrandMapper.java
@@ -0,0 +1,9 @@
+package com.cyan.mapper;
+
+import com.cyan.pojo.Brand;
+
+import java.util.List;
+
+public interface BrandMapper {
+    public List<Brand> selectAll();
+}
--- src/test/java/com/cyan/test/MyBatisTest.java
+++ src/test/java/com/cyan/test/MyBatisTest.java
@@ -1,5 +1,35 @@
 package com.cyan.test;
 
-public class MyBatisTest {
+import com.cyan.mapper.BrandMapper;
+import com.cyan.pojo.Brand;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
 
+import java.io.InputStream;
+import java.util.List;
+
+public class MyBatisTest {
+    @Test
+    public void testSelectAll() throws Exception{
+        //1. 加载MyBatis 配置文件，获取SqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        //2. 获取sqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        //3. 获取mapper代理对象
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+
+        //4. 执行方法
+        List<Brand> brands = brandMapper.selectAll();
+        System.out.println(brands);
+
+        //5. 释放资源
+        sqlSession.close();
+    }
 }
