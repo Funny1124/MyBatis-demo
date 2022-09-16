8593e727e7af5e8cf713512087e58ee17d0d695d
Cyan <204267667@qq.com>
2022-09-16 23:52:44 +0800

    select brand By id

--- src/main/java/com/cyan/mapper/BrandMapper.java
+++ src/main/java/com/cyan/mapper/BrandMapper.java
@@ -5,5 +5,16 @@ import com.cyan.pojo.Brand;
 import java.util.List;
 
 public interface BrandMapper {
-    public List<Brand> selectAll();
+    /**
+     * 查询所有
+     * @return 品牌列表
+     */
+    List<Brand> selectAll();
+
+    /**
+     * 根据id查询
+     * @param id 品牌id
+     * @return 品牌详情
+     */
+    Brand selectById(int id);
 }
--- src/test/java/MyBatisTest.java
+++ src/test/java/MyBatisTest.java
@@ -1,16 +1,19 @@
+import com.cyan.mapper.BrandMapper;
 import com.cyan.mapper.UserMapper;
+import com.cyan.pojo.Brand;
 import com.cyan.pojo.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
-public class MyBatisDemo2 {
-    public static void main(String[] args) throws IOException {
+public class MyBatisTest {
+    @Test
+    public void testSelectAll() throws Exception {
         //1.加载MyBatis 配置文件，获取SqlSessionFactory
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
@@ -27,6 +30,24 @@ public class MyBatisDemo2 {
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
         List<User> users = userMapper.selectAll();
         System.out.println(users);
+        //4.释放资源
+        sqlSession.close();
+    }
+
+    @Test
+    public void testSelectById() throws Exception {
+        int id = 2;
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        //2.获取SqlSession对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        //代理模式
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+        Brand brand = brandMapper.selectById(id);
+        System.out.println(brand);
         //4.释放资源
         sqlSession.close();
     }
