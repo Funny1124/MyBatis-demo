befc63f15e70e50e52633117984a573e3eb831c2
Cyan <204267667@qq.com>
2022-09-17 14:50:50 +0800

    条件查询（模糊查询）

--- src/test/java/MyBatisTest.java
+++ src/test/java/MyBatisTest.java
@@ -8,8 +8,11 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MyBatisTest {
     @Test
@@ -49,6 +52,54 @@ public class MyBatisTest {
         Brand brand = brandMapper.selectById(id);
         System.out.println(brand);
         //4.释放资源
+        sqlSession.close();
+    }
+
+//    若查询结果为空集，
+//    检查sql是否拼接错误，
+//    可能数据库连接的字符编码有误，连接信息添加：useUnicode=true&amp;characterEncoding=UTF-8
+    @Test
+    public void testSelectByCondition() throws IOException {
+        //接收参数
+        int status = 1;
+        String companyName = "华为";
+        String brandName = "华为";
+
+        // 处理参数
+        companyName = "%" + companyName + "%";
+        brandName = "%" + brandName + "%";
+
+        //1. 获取SqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        //2. 获取SqlSession对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        //3. 获取Mapper接口的代理对象
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+
+        //4. 执行方法
+        //方式一 ：接口方法参数使用 @Param 方式调用的方法
+//        List<Brand> brands = brandMapper.selectByCondition(status, companyName, brandName);
+        //方式二 ：接口方法参数是 实体类对象 方式调用的方法
+        //封装对象
+//        Brand brand = new Brand();
+//        brand.setStatus(status);
+//        brand.setCompanyName(companyName);
+//        brand.setBrandName(brandName);
+//
+//        List<Brand> brands = brandMapper.selectByCondition(brand);
+
+        //方式三 ：接口方法参数是 map集合对象 方式调用的方法
+        Map<String, java.io.Serializable> map = new HashMap<String, java.io.Serializable>();
+        map.put("status" , status);
+        map.put("companyName", companyName);
+        map.put("brandName" , brandName);
+        List<Brand> brands = brandMapper.selectByCondition(map);
+
+        System.out.println(brands);
+
+        //5. 释放资源
         sqlSession.close();
     }
 }
